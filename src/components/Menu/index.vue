f3d6b0071f8efe605023f7ee10dbbcf91dde4178
evan4 <evan4mc@gmail.com>
2019-03-17 14:05:55 +0500

    finish menu

--- src/components/Menu/index.vue
+++ src/components/Menu/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="conyainer menu_container">
+    <div class="container menu_container">
         <div class="top">
             <h2>Menu</h2>
         </div>
@@ -11,6 +11,23 @@
             @click="changeMenu(button.name)">
             {{button.name}}
             </md-button>
+        </div>
+        <div class="products-items">
+            <md-card
+                v-for="(product, index) in products" :key="index"
+                v-show="product.type === activeProduct">
+                <md-card-media md-ratio="16:9">
+                    <img :src="require(`../../assets/images/products/${product.img}`)" 
+                    :alt="product.name">
+                </md-card-media>
+                <md-card-header>
+                    <h2 class="md-title">{{product.name}}</h2>
+                    <div class="md-subhead">
+                        <span>$ {{product.price}}</span>
+                    </div>
+                </md-card-header>
+                <md-card-content>{{product.desc}}</md-card-content>
+            </md-card>
         </div>
     </div>
 </template>
@@ -37,6 +54,7 @@ export default {
                     active: false
                 }
             ],
+            activeProduct: 'pizza',
             products: []
         }
     },
@@ -45,7 +63,8 @@ export default {
             for (let index = 0; index < this.buttons.length; index++) {
                 const element = this.buttons[index];
                 if(element.name === name){
-                    element.active = true
+                    element.active = true;
+                    this.activeProduct = name;
                 }else{
                     element.active = false
                 }
@@ -77,6 +96,14 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    .md-card{
+        width: 32%;
+        margin-bottom: 20px;
+    }
+    .products-items{
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
 </style>
